8f1702f75f1a45960bde85872ccda67f104a312e
a5436 <150577151+a5436@users.noreply.github.com>
2026-09-14 03:38:11 +0800

    Apply TK跨境系统 branding

--- devices/mediatek_filogic/diy.sh
+++ devices/mediatek_filogic/diy.sh
@@ -5,7 +5,7 @@ SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
 sed -i -E -e 's/ ?root=\/dev\/fit0 rootwait//' -e "/rootdisk =/d" -e '/bootargs.* = ""/d' target/linux/mediatek/dts/*{qihoo-360t7,netcore-n60*,h3c-magic-nx30-pro,jdcloud-re-cp-03,cmcc-rax3000m,jcg-q30-pro,tplink-tl-xdr*,abt-asr3000,komi-a31,nokia-ea0326gmp,bt-r320}*.dts*
 
-# TK跨境 root filesystem overlay (contains no user nodes, MACs or credentials)
+# TK跨境系统 root filesystem overlay (contains no user nodes, MACs or credentials)
 mkdir -p files
 base64 -d "$SHELL_FOLDER/tk-crossborder-overlay.tar.gz.b64" > /tmp/tk-crossborder-overlay.tar.gz
 tar -xzf /tmp/tk-crossborder-overlay.tar.gz -C files
@@ -13,5 +13,5 @@ rm -f /tmp/tk-crossborder-overlay.tar.gz
 chmod 0755 files/etc/uci-defaults/99-tk-crossborder files/etc/init.d/passwall-smart-incremental files/usr/libexec/passwall-smart/*
 
 # Branding and first-boot LAN address
-sed -i 's/Kwrt/TK跨境/g' package/base-files/files/bin/config_generate package/base-files/image-config.in config/Config-images.in include/version.mk 2>/dev/null || true
+sed -i 's/Kwrt/TK跨境系统/g' package/base-files/files/bin/config_generate package/base-files/image-config.in config/Config-images.in include/version.mk 2>/dev/null || true
 sed -i 's/10\.0\.0\.1/10.5.0.1/g' package/base-files/files/bin/config_generate 2>/dev/null || true
